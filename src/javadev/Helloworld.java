35e71aeab36187e1e54ea444219cfcc36070f9a1
HTTruongthietkeweb <4901104162@student.hcmue.edu.vn>
2024-08-04 18:43:26 +0700

    init project

---
+++ src/javadev/Helloworld.java
@@ -0,0 +1,12 @@
+package javadev;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+
+	}
+}
